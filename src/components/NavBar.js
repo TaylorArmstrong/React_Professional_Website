d3e8541bfb843feb8092b5bb63bf76cbf28bc0c5
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-01-14 11:21:24 -0700

    email modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,8 @@ import workSpaceImage from '../images/deskImage.jpg'
 const NavBar = () => {
     return (
         <div>
-            <Navbar brand='logo' className='hide-on-med-and-down' right>
+            <Navbar className='hide-on-med-and-down' right>
+                <NavItem>myLogo</NavItem>
                 <NavItem href='#top' >WELCOME</NavItem>
                 <NavItem href='#projects' >PROJECTS</NavItem>
                 <NavItem href='#about' >ABOUT</NavItem>
@@ -53,57 +54,3 @@ const NavBar = () => {
 }
 
 export default NavBar
-
-
-
-// <div>
-        //     <nav className='nav-extended'>
-        //         <div className='nav-wrapper'>
-        //             <a href="#" data-target="mobile-demo" className="sidenav-trigger hide-on-large-only"><i class="material-icons">menu</i></a>
-        //             <ul className='left hide-on-med-and-down'>
-        //                 <li className='nav-item nav-last'>
-        //                     <a className='nav-link' href='#top'>Logo</a>
-        //                 </li>
-        //             </ul>
-        //             <ul className='right hide-on-med-and-down'>
-        //                 <li className='nav-item nav-last'>
-        //                     <a className='nav-link' href='#top'>WELCOME</a>
-        //                 </li>
-        //                 <li className='nav-item'>
-        //                     <a className='nav-link' href='#projects'>PROJECTS</a>
-        //                 </li>
-        //                 <li className='nav-item'>
-        //                     <a className='nav-link' href='#about'>ABOUT</a>
-        //                 </li>
-        //                 <li className='nav-item'>
-        //                     <a className='nav-link' href='#technologies'>TECHNOLOGIES</a>
-        //                 </li>
-        //                 <li className='nav-item'>
-        //                     <a className='nav-link' href='#contact'>CONTACT</a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </nav>
-        //     <ul id='mobile-demo' className='sidenav hide-on-large-only'>
-        //         <li><a className='nav-link' href='#top'>WELCOME</a></li>
-        //         <li>
-        //             <a className='nav-link collapsible-header' href='#projects'>PROJECTS</a>
-        //             <a className='collapsible-body' id='projectGitIcon' href='https://github.com/TaylorArmstrong'><i className='fab fa-github'></i>GitHub</a>
-        //         </li>
-        //         <li>
-        //             <a className='nav-link collapsible-header' href='#about'>ABOUT</a>
-        //             <a className='collapsible-body' href='http://www.linkedin.com/in/taylorarmstrong13'><i className='fab fa-linkedin'></i>LinkedIn</a>
-        //             <a className='collapsible-body' href='https://talent.galvanize.com/students/3092'><i className='far fa-user-circle'></i>Galvanize Talent</a>
-        //         </li>
-        //         <li><a className='nav-link' href='#technologies'>TECHNOLOGIES</a></li>
-        //         <li>
-        //             <a className='nav-link collapsible-header' href='#contact'>CONTACT
-        //                 </a>
-        //             <a className='collapsible-body' title='Call Taylor @ (203)-613-3326' id='Cell Number' href='tel:1-203-613-3326'><i className='material-icons'>phone</i>(203)-613-3326</a>
-        //             <a className='collapsible-body' title='E-Mail Taylor @ TArmstrong1327@gmail.com' id='E-mail' href='mailto:TArmstrong1327@gmail.com'><i className='material-icons'>email</i>TArmstrong1327@gmail.com</a>
-        //         </li>
-        //         <li className='nav-item nav-last'>
-        //             <a className='nav-link' href='#top'>Logo</a>
-        //         </li>
-        //     </ul>
-        // </div>
