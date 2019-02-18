a8c1730181d626deac8b6da9c1eeacab4616a05c
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-02-18 13:21:16 -0700

    working on style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,27 +5,21 @@ const Projects = () => {
     return (
         <div id='projects' className='center'>
             {ProjectInfo.map(x =>
-            <div key={x.title} className='tc bg-light-blue br3 pa3 ma2 dib bw2 shadow-5 small' id='ProjectCard'>
-                <div className='f4 lh-copy'><b><u>{x.title}</u></b><br /></div>
+            <div key={x.title} style={{width: 415}} className='tj bg-light-blue br3 pa3 ma2 dib bw2 shadow-5 small' id='ProjectCard'>
+                <div className='tc f4 lh-copy'><b><u>{x.title}</u></b><br /></div>
                 {x.description}
-                    {!x.url ? 
-                        <div>
-                            <br /> 
-                            <em>{x.languages}</em><br /><br />
-                            <u><b>GitHub Repository</b></u><br /><br /><a href={x.github}>{x.github}</a><br /><br /><br /><br />
-                        </div> 
-                            : 
-                            <div>
-                                <div> 
-                                    <em>{x.languages}</em><br /><br />
-                                    <u><b>GitHub Repository</b></u><br /><a href={x.github}>{x.github}</a><br /><br />
-                                </div>
-                                <div>
-                                    <u><b>Url:</b></u><a href={x.url}> {x.url}</a><br /><br />
-                                </div>
-                            </div>
-                        }
-                    {!x.demo ? '' : <a href={x.demo}><u><b>Demo Video</b></u></a>}
+                    <div>
+                        <br /> 
+                        <em>{x.languages}</em><br /><br />
+                        <i className='fab fa-github' id='headerIcon' /><a href={x.github}>{x.github}</a><br /><br /><br />
+                    {!x.url 
+                    ? '' 
+                    :<div>
+                        <u><b>Url:</b></u> <a href={x.url}> {x.url}</a> <br /> <br />
+                    </div>
+                    }
+                    </div>     
+                {!x.demo ? '' : <div className='tc'><a href={x.demo}><u><b>Demo Video</b></u></a></div>}
             </div>
             )}
         </div>
