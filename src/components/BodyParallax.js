9029c057653028272f452d9b79e015480fb440f5
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-03-02 17:38:40 -0700

    styling header

--- src/components/BodyParallax.js
+++ src/components/BodyParallax.js
@@ -3,9 +3,7 @@ import { Parallax } from "react-parallax"
 
 const BodyParallax = () => {
     return (
-        <Parallax style={{ 
-                'background-repeat': 'no-repeat',
-                'background - size': 'cover'}} 
+        <Parallax 
             bgImage={require('../images/redRocksOutsideBanner.jpg')} strength={500}>
             <div style={{ height: 500 }}>
             </div>
